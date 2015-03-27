c6ace483e0dc85626309bcfe4f1f6964382247a2
Richard Wu <contactrichardwu@gmail.com>
2015-03-28 01:26:43 +0800

    Some css change + start with the plan

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//The user clicks in the title section
+//The title should save to the dom
+
+// The user Add an item
+//The mouse should go 
+
+$(document).ready(function(){
+  console.log("ready!");
+});
