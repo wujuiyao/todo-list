ac35565aee94a1a07c2eac7b1d7891701166fae5
Richard Wu <contactrichardwu@gmail.com>
2015-03-28 13:35:41 +0800

    ...

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@
 $(document).ready(function(){
   console.log("ready!");
   //By default show only 2 examples. Use jquery code to this
-  $('.selected').hide();
-  $('.standard').hide();
-  $('.active').hide();
+  // $('.selected').hide();
+  // $('.standard').hide();
+  // $('.active').hide();
   //The user clicks on the "Add Item"
   $('.add-item').click(function(){
     //The active (html) need to show up
     $('.active').show();
     //the mouse cursor should be directly in the active version.
-    
+
   });
 });
