f0f6fe047fde90c9f96aff4953c23092e0b93fe6
Richard Wu <contactrichardwu@gmail.com>
2015-03-28 13:34:41 +0800

    ...

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-//The user clicks in the title section
-//The title should save to the dom
+//What to do with Add item
 
-// The user Add an item
-//The mouse should go 
 
 $(document).ready(function(){
   console.log("ready!");
+  //By default show only 2 examples. Use jquery code to this
+  $('.selected').hide();
+  $('.standard').hide();
+  $('.active').hide();
+  //The user clicks on the "Add Item"
+  $('.add-item').click(function(){
+    //The active (html) need to show up
+    $('.active').show();
+    //the mouse cursor should be directly in the active version.
+    
+  });
 });
