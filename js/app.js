9fbcac1c0d3eb8c0b2854bcf07ef9eece2c14b11
Richard Wu <contactrichardwu@gmail.com>
2015-03-29 22:18:24 +0800

    Removed some js code

--- js/app.js
+++ js/app.js
@@ -3,34 +3,22 @@
 
 $(document).ready(function(){
   console.log("ready!");
-  //the html first
+  //hide the initial html
   $('.selected').hide();
   $('.standard').hide();
   //$('.active').hide();
-  // tryout one
-  //Capture the click event on the "Add Item" Button
-    //  $('.add-item').click(function(){
-    //    console.log(newList);
-    //    //Add the first active list
-    //    //Add more lists on more clicks
-    //    $(this).on('click', '.active li', function(){
-    //      $('.active').append($('.active li'));
-    //    });
-    // });
 
-  // //tryout 2
-  // $('.add-item').click(function(){
-  //   $('.active').append($('.active li'));
-  // });
-
-  // tryout 3
-  //Add Item Function
-  function addNewItem(){
-    $('.active').append('<li><form action=""><input type="checkbox" name="" value="item"><input type="text" name="" value="New Items"></form><div id="dropdown"><form action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></form><form action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></form></div>');
-  }
+  // 1.Add an New Item (detailed)
   $('.add-item').click(function(){
-    addNewItem();
+    console.log('Add Item clicked');
+    $('.active').append('<li><form action=""><input type="checkbox" name="" value="item"><input type="text" name="" value="New Items"></form><div id="dropdown"><form action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></form><form action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></form></div>');
   });
+    //1.1 Focus the Mouse Cursor in 'New Item' input (always the latest Add Item click)
+    //1.1.1 The users click multiple time on the 'Add-Item' Button.
+          //Only the latest Item will with all details and the rest item should be the standard
+      //1.2 If the user clicks outside the 'New Item' content, the detailed info fades away.
+      //1.2.1 and if the user clicks on the "Done" the detailed info fades away
+      
   //use prepend to add the x arrow if they click on the selected
   //use focus jquery api
 });
