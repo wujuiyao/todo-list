a5e2e501cb22665d17c5560b804074cc1dab5bdf
Richard Wu <contactrichardwu@gmail.com>
2015-03-31 00:43:18 +0800

    LEarn jquery

--- js/app.js
+++ js/app.js
@@ -11,9 +11,14 @@ $(document).ready(function(){
 //   1. Add a ‘New List’ (by clicking on the Add-Item Button)
   $('.add-item').click(function(){
     console.log('Add Item clicked');
-    $('.active').append('<li><form action=""><input type="checkbox" name="" value="item"><input type="text" name="" value="New Items"></form><div id="dropdown"><form action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></form><form action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></form></div>');
+    //1.1 Set a var for the all the added list
+    var numList = val();
+    $('.active').append('<li><input type="checkbox" name="" value="item"><input type="text" name="" value="New Items"><div id="dropdown"><p action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></p><p action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></p></div>')
+    .css("border-bottom", "2px solid #e8e8e8");
+    //1.2 Mouse should be focused on the ‘New Item Text Input Bar’
+    $('.active [value="New Item"]').focus(); // to the last added item
   });
-// 1.1 Mouse should be focused on the ‘New Item Text Input Bar’
+
 // 1.1.1 User clicks on multiple time on the ‘Add-Item’ Button -> It will Create a ‘New List’
 // 1.1.2 Previous created List will only show the basic (that means not the detailed version)
 //
@@ -26,4 +31,15 @@ $(document).ready(function(){
 
   //use prepend to add the x arrow if they click on the selected
   //use focus jquery api
+
+  //mouseenter
+  //click
+  //dbclick
+  //focusin()
+  //focusout()
+  //hover()
+  //toggle
+  //use the val() to create a max item list
+
+  //slideUp();
 });
