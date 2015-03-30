9eb80f80029cc316ef71b6845e3b73301a732f3a
Richard Wu <contactrichardwu@gmail.com>
2015-03-30 22:34:27 +0800

    Add comments in app.js

--- js/app.js
+++ js/app.js
@@ -8,17 +8,22 @@ $(document).ready(function(){
   $('.standard').hide();
   //$('.active').hide();
 
-  // 1.Add an New Item (detailed)
+//   1. Add a ‘New List’ (by clicking on the Add-Item Button)
   $('.add-item').click(function(){
     console.log('Add Item clicked');
     $('.active').append('<li><form action=""><input type="checkbox" name="" value="item"><input type="text" name="" value="New Items"></form><div id="dropdown"><form action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></form><form action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></form></div>');
   });
-    //1.1 Focus the Mouse Cursor in 'New Item' input (always the latest Add Item click)
-    //1.1.1 The users click multiple time on the 'Add-Item' Button.
-          //Only the latest Item will with all details and the rest item should be the standard
-      //1.2 If the user clicks outside the 'New Item' content, the detailed info fades away.
-      //1.2.1 and if the user clicks on the "Done" the detailed info fades away
-      
+// 1.1 Mouse should be focused on the ‘New Item Text Input Bar’
+// 1.1.1 User clicks on multiple time on the ‘Add-Item’ Button -> It will Create a ‘New List’
+// 1.1.2 Previous created List will only show the basic (that means not the detailed version)
+//
+// 1.2 The user clicks out of the ‘Item List’ area
+// 1.2.1 The detailed part will go away
+//
+// 1.3 The user clicks on the “Done” Button
+// 1.3.1 The detailed part will go away
+
+
   //use prepend to add the x arrow if they click on the selected
   //use focus jquery api
 });
