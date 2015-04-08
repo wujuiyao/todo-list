e937741fe19041a048b2dce84cb08ffaa044287e
Richard Wu <contactrichardwu@gmail.com>
2015-04-08 23:52:11 +0800

    Add more jquery

--- js/app.js
+++ js/app.js
@@ -13,11 +13,24 @@ $(document).ready(function(){
     console.log('Add Item clicked');
     //1.1 Set a var for the all the added list
     var newList = $('.active li').length;
+    //Set var for current list
+    var currentList = newList;
+    //Set var for prev list
+    var prevList = newList-1;
+    console.log(prevList);
     //1.2 Set a max list of 6
     if(newList <= 5){
-      $('.active').append('<li><input type="checkbox" name="" value="item" id="item-'+newList+'"><input type="text" name="" value="New Items"><div id="dropdown"><p action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></p><p action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></p></div></li>')
+      $('.active').append('<li><input type="checkbox" name="" value="item" id="item-'+newList+'"><input type="text" name="" value="New Items" id="target"><div id="dropdown"><p action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></p><p action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></p></div></li>')
       .css("border-bottom", "2px solid #e8e8e8");
       console.log($('.active #item-'+newList+''));
+      // 1.1.2 Previous created List will only show the basic
+      if(prevList === 0){
+        console.log("something should happen in list -1");
+      }else if(prevList === 1){
+        console.log("something should happen in list 0");
+      }else if(prevList === 2){
+        console.log("something should happen in list 1");
+      }
     }
     //1.3 Create an Alert that you can only set 6 List for now
     var count = 0;
@@ -28,11 +41,11 @@ $(document).ready(function(){
       }
     });
     //1.1.1 Mouse should be focused on the ‘New Item Text Input Bar’
-    $('.active [value="New Item"]').focus(); // to the last added item
+    $('.active #target').focus();
+
   });
 
-// 1.1.2 User clicks on multiple time on the ‘Add-Item’ Button -> It will Create a ‘New List’
-// 1.1.3 Previous created List will only show the basic (that means not the detailed version)
+
 //
 // 1.2 The user clicks out of the ‘Item List’ area
 // 1.2.1 The detailed part will go away
