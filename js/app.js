5c7caebb97ba5de85690cbb0cbf0dc0595ae787e
Richard Wu <contactrichardwu@gmail.com>
2015-04-09 23:00:35 +0800

    Try the jquery

--- js/app.js
+++ js/app.js
@@ -13,25 +13,28 @@ $(document).ready(function(){
     console.log('Add Item clicked');
     //1.1 Set a var for the all the added list
     var newList = $('.active li').length;
+    console.log('item'+-newList);
     //Set var for current list
     var currentList = newList;
     //Set var for prev list
     var prevList = newList-1;
-    console.log(prevList);
     //1.2 Set a max list of 6
     if(newList <= 5){
       $('.active').append('<li><input type="checkbox" name="" value="item" id="item-'+newList+'"><input type="text" name="" value="New Items" id="target"><div id="dropdown"><p action="" class="quantity">Quantity:<input type="text" name="quantity" value=""></p><p action="" class="notes"><input type="text" name="notes" value="Notes:"><button>Done</button></p></div></li>')
       .css("border-bottom", "2px solid #e8e8e8");
       console.log($('.active #item-'+newList+''));
-      // 1.1.2 Previous created List will only show the basic
-      if(prevList === 0){
-        console.log("something should happen in list -1");
-      }else if(prevList === 1){
-        console.log("something should happen in list 0");
-      }else if(prevList === 2){
-        console.log("something should happen in list 1");
+      //1.1.1 Mouse should be focused on the ‘New Item Text Input Bar’
+      $('.active #target').focus();
+      // 1.1.2 Previous created List will only show the basic list
+      if(newList === 1){
+        $('.active').hide('#item0 #dropdown');
+      }else if(newList === 2){
+      }else if(newList === 3){
+      }else if(newList === 4){
       }
+
     }
+
     //1.3 Create an Alert that you can only set 6 List for now
     var count = 0;
     $('.add-item').click(function(){
@@ -40,8 +43,6 @@ $(document).ready(function(){
         alert("Sorry you can only have 6 Items for now!");
       }
     });
-    //1.1.1 Mouse should be focused on the ‘New Item Text Input Bar’
-    $('.active #target').focus();
 
   });
 
