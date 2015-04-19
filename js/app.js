574a973f3641a4cea3733c9292ae0f32e0ad0b77
Richard Wu <contactrichardwu@gmail.com>
2015-04-19 21:25:23 +0800

    fixed and removed some htnk and  jquery

--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
 $(document).ready(function(){
-  //hide the initial html
-  $('.selected').hide();
-  $('.standard').hide();
-  $('.active #dropdown').hide();
+
   var dropdown = '<div id="dropdown"><p class="quantity">Quantity:<input type="text" name="quantity" value=""></p><p class="notes"><input type="text" name="notes" value="Notes:"><button id="done">Done</button></p></div>';
 
-  // Add a ‘New List’ (by clicking on the Add-Item Button)
-    $('.add-item').on('click', function(){
-      addNewList();
-    });
-  //*** Function: Add new List
-  function addNewList(){
-  // list variable
-  var newList = $('.active li').length;
-    if(newList <= 5){
-      console.log($('.active #item-'+newList+''));
-      console.log($('.active #target-'+newList+''));
-      $('.active').append('<li><input type="checkbox" name="" value="item" id="item-'+newList+'"><input type="text" name="" value="New Items" id="target-'+newList+'"></li><br>');
-      $(dropdown).appendTo($('.active'));
-      $('.active #target-'+newList+'').focus();
-    }
-    //jquery that activates the details, delegate jquery
-    //Show details of each item
-    $('.active').on('dblclick','li',function(){
-     $('.active #dropdown').show().appendTo(this);
-    });
 
-    //Checked the tickbox
-    //use the val from jquery
-    $('.active').on('checked','input:checkbox', function(){
-      $('.active li').append('<button><img src="images/delete.png" alt=""></button>');
-     });
-  }
+  $('.add-item').on('click.createList', function(){
+
+    var newList = $('.active li').length;
+    var list ='<li id="item-'+newList+'"><input  type="checkbox" name="" value="item" ><input id="target-'+newList+'" type="text" name="" value="New Items" ></li><br>';
+
+    if(newList <= 5){
+      $('.active').append(list);
+      $(dropdown).appendTo($('.active'));
+      $('.active [value="New Items"]').focus();
+    }
+    if(newList >=5){
+      $('.add-item').off('click.createList');
+      alert("no more than 6 please");
+    }
+  });
+
+
+
+
+
+
+
+
+  //jquery that activates the details, delegate jquery
+  //Show details of each item
+  $('.active').on('dblclick','li',function(){
+   $('.active #dropdown').show().appendTo(this);
+  });
+
+  //Checked the tickbox
+  //use the val from jquery
+  $('.active').on('checked','input:checkbox', function(){
+    $('.active li').append('<button><img src="images/delete.png" alt=""></button>');
+   });
 
 //*** Function: Hide Details
   $('#done').click(function(){
@@ -49,6 +54,8 @@ $(document).ready(function(){
 // 1.3.1 The detailed part will go away
 
 
+  // use this to check if is true of false prop = property
+  //$('...').prop()
   //use prepend to add the x arrow if they click on the selected
   //use focus jquery api
 
