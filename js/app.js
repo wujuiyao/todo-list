005127476a56f13bb6f5aedcb2418eb680915325
Richard Wu <contactrichardwu@gmail.com>
2015-04-22 00:09:46 +0800

    jquery from scratch

--- js/app.js
+++ js/app.js
@@ -4,22 +4,25 @@ $(document).ready(function(){
 
   $('.add-item').on('click', addItem);
 
+
   function addItem(){
     if (maxAppend >= 6) return;
     var newList = $('#item-'+newList+'').length;
     var dropdownList = $('.active .dropdown-'+newList+'').length;
-    var list =$('<li id="item-'+newList+'"><input type="checkbox" name="" value="item" ><input id="target-'+newList+'" type="text" name="" value="New Items" ></li><br>');
-    var dropdown =$('<div class="dropdown-'+newList+'" id="dropdown"><p class="quantity">Quantity:<input type="text" name="" value=""></p><p class="notes"><input type="text" name="" value="Notes:"><button id="done">Done</button></p></div>');
+    var list =$('<li id="item-'+newList+'"><input type="checkbox" name="" value="item" ><input id="target-'+newList+'" type="text" name="" value="New Items" ></li>');
+    var dropdown =$('<div class="dropdown-'+newList+'" id="dropdown"><p class="quantity">Quantity:<input type="text" name="" value=""></p><p class="notes"><input type="text" name="" value="Notes:"><button>Done</button></p></div>');
     var wholeList = $(list).add(dropdown);
                     maxAppend++;
 
-    $('.active').append(list);
+    $('.active').append(wholeList);
+    //use each to iterate over these and add new classes
+    //generating class dynamically in jquery
 
-    //use jquery to find the latest li and add the dropdown to it
-    $(dropdown).appendTo(list);
     $('.active [value="New Items"]').val();
     $('.active [value="New Items"]').focus();
   }
+
+  //argument passing
 
 //once the the add has been clciked return the list number
 
