6e1e2c78e02e79ef835a38724e933834b40cfbbd
Richard Wu <contactrichardwu@gmail.com>
2015-04-24 20:35:13 +0800

    adding dynamic classes

--- js/app.js
+++ js/app.js
@@ -1,47 +1,40 @@
 $(document).ready(function(){
 
   var maxAppend = 0;
+  var buttonArr = ['.button-0','.button-1','.button-2','button-3','button-4','button-5'];
 
-  $('.add-item').on('click', addItem);
 
+  $('.add-item').click(addItem);
+  $.each( buttonArr, function( i, bA ){
+     alert( "Index" + i + ": " + bA );
+
+
+  });
+
+  //try one using each, one using index and one using idea from Nicholas.
 
   function addItem(){
     if (maxAppend >= 6) return;
-    var newList = $('#item-'+newList+'').length;
-    var dropdownList = $('.active .dropdown-'+newList+'').length;
-    var list =$('<li id="item-'+newList+'"><input type="checkbox" name="" value="item" ><input id="target-'+newList+'" type="text" name="" value="New Items" ></li>');
-    var dropdown =$('<div class="dropdown-'+newList+'" id="dropdown"><p class="quantity">Quantity:<input type="text" name="" value=""></p><p class="notes"><input type="text" name="" value="Notes:"><button>Done</button></p></div>');
+
+    var list =$('<li><input type="checkbox" name="" value="item" ><input type="text" name="" value="New Items" ></li>');
+    var dropdown =$('<div id="dropdown"><p class="quantity">Quantity:<input type="text" name="" value=""></p><p class="notes"><input type="text" name="" value="Notes:"><button>Done</button></p></div>');
     var wholeList = $(list).add(dropdown);
                     maxAppend++;
 
-    $('.active').append(wholeList);
-    //use each to iterate over these and add new classes
-    //generating class dynamically in jquery
-
-    $('.active [value="New Items"]').val();
+    $('.active').append(list);
+    $(dropdown).appendTo(list);
+    //** adding classes for further reference
+    $('.active li').addClass(function(index){
+      return "item-" + index;
+    });
+    $('.active li').find('div').addClass(function(index){
+      return "dropdown-" + index;
+    });
+    $('.active li').find('button').addClass(function(index){
+      return "button-" + index;
+    });
+    //**End
+    // Focus on the added Item
     $('.active [value="New Items"]').focus();
   }
-
-  //argument passing
-
-//once the the add has been clciked return the list number
-
 });
-
-//$('.active').on("click","li", function(evt){
-//    alert($(this).text());
-//});
-
-  // use this to check if is true of false prop = property
-  //$('...').prop()
-  //use prepend to add the x arrow if they click on the selected
-  //use focus jquery api
-
-  //mouseenter
-  //click
-  //dbclick
-  //focusin()
-  //focusout()
-  //hover()
-  //toggle
-  //use the val() to create a max item list
